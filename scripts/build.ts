46dbcc49883f34ef339e6ce68891b8902d962a99
Phumrapee Limpianchop <rayriffy@gmail.com>
2017-12-16 11:11:46 +0700

    build: update build script (#29)
    PR Close #29

--- scripts/build.ts
+++ scripts/build.ts
@@ -30,40 +30,40 @@ console.log('Copying files');
 ensureDirSync(buildPath);
 
 const files = [
-  '01-INFO',
-  '02-VOR',
-  '03-NDB',
-  '04-FIXES',
-  '05-AIRPORT',
-  '06-RWY',
-  '09-HI_AIRWAY',
-  '10-LO_AIRWAY',
-  '12-ARTCC_HI'
+  '01-INFO.txt',
+  '02-VOR.txt',
+  '03-NDB.txt',
+  '04-FIXES.txt',
+  '05-AIRPORT.txt',
+  '06-RWY.txt',
+  '09-HI_AIRWAY.txt',
+  '10-LO_AIRWAY.txt',
+  '12-ARTCC_HI.txt'
 ];
 
 const folders = [
-  ['SID', '07-SID'],
-  ['STAR', '08-STAR'],
-  ['ARTCC', '11-ARTCC'],
-  ['ARTCC_LO', '13-ARTCC_LO'],
-  ['GEO', '14-GEO']
+  ['SID', '07-SID.txt'],
+  ['STAR', '08-STAR.txt'],
+  ['ARTCC', '11-ARTCC.txt'],
+  ['ARTCC_LO', '13-ARTCC_LO.txt'],
+  ['GEO', '14-GEO.txt']
 ];
 
 const orders = [
-  '01-INFO',
-  '02-VOR',
-  '03-NDB',
-  '04-FIXES',
-  '05-AIRPORT',
-  '06-RWY',
-  '07-SID',
-  '08-STAR',
-  '09-HI_AIRWAY',
-  '10-LO_AIRWAY',
-  '11-ARTCC',
-  '12-ARTCC_HI',
-  '13-ARTCC_LO',
-  '14-GEO'
+  '01-INFO.txt',
+  '02-VOR.txt',
+  '03-NDB.txt',
+  '04-FIXES.txt',
+  '05-AIRPORT.txt',
+  '06-RWY.txt',
+  '07-SID.txt',
+  '08-STAR.txt',
+  '09-HI_AIRWAY.txt',
+  '10-LO_AIRWAY.txt',
+  '11-ARTCC.txt',
+  '12-ARTCC_HI.txt',
+  '13-ARTCC_LO.txt',
+  '14-GEO.txt'
 ];
 
 for (let file of files) {
