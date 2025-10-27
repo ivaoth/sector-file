1322ab4d1547f36de8745dde96e57a96790ff8e7
Tatpol Chutipat <tatpolchutipat@gmail.com>
2025-10-28 05:07:09 +0700

    feat(aurora/VTBB): Parse and Build Overhaul
    airway.ts
    Add boundary files to support boundary fix.
    
    Added filtering to accept domestic airway with to/from VT region.
    
    ndb.ts
    Add is_extra filter to hide extra NDB.
    
    vor.ts
    Add is_extra filter to hide extra VOR.
    
    Add VOR filter to detect TACAN, VOR only, DME only.
    
    waypoint.ts
    Add boundary filter to show boundary fixes.
    
    Filter out database error for SV###.
    
    aurora.ts
    Add scripts to support hidden NDB and VOR.
    
    Add scripts to support VOR types (TACAN, VOR only, DME only).
    
    Added support to TACAN channel.
    
    formatAirways.ts
    Added scripts to generate label within VT region only.

--- airac-parser/common/ndb.ts
+++ airac-parser/common/ndb.ts
@@ -12,8 +12,24 @@ interface NdbDbData {
 
 export const extractNdbs = async (
   db: Promise<Database>,
-  extra: number[]
+  extra: number[],
+  boundary: number[]
 ): Promise<Ndb[]> => {
+
+  const boundarySet = new Set(boundary);
+
+  const addIsExtra =
+    (isExtra: boolean) =>
+    (s: NdbDbData[]): Ndb[] => {
+      return s.map((w) => {
+        const { ...others } = w;
+        return {
+          ...others,
+          is_extra: isExtra
+        };
+      });
+    };
+
   const ndbs: Promise<Ndb[]> = (await db).all<NdbDbData[]>(SQL`
     SELECT
     ident, name, frequency, laty, lonx
@@ -21,25 +37,26 @@ export const extractNdbs = async (
     ndb
     WHERE
     region = 'VT'
-  `);
-  const ids = `(${extra.join(',')})`;
-  const extraNdbs: Promise<Ndb[]> = (await db).all<NdbDbData[]>(
-    SQL`
-    SELECT
-    N.ident, N.name, N.frequency, N.laty, N.lonx
-    FROM
-    (
-      waypoint W
-      INNER JOIN
-      ndb N
-      ON W.nav_id = N.ndb_id
-    )
-    WHERE
-    w.waypoint_id IN
-  `.append(ids).append(SQL`
-    AND
-    W.type = 'N'
   `)
-  );
+  .then(addIsExtra(false));
+
+  const ids = `(${extra.join(',')})`;
+  
+  const extraNdbs: Promise<Ndb[]> = (await db)
+    .all<NdbDbData[]>(
+      SQL`
+        SELECT
+          N.ident, N.name, N.frequency, N.laty, N.lonx
+        FROM
+          waypoint W
+          INNER JOIN ndb N ON W.nav_id = N.ndb_id
+        WHERE
+          W.waypoint_id IN
+      `.append(ids).append(SQL`
+          AND W.type = 'N'
+      `)
+    )
+    .then(addIsExtra(true));
+    
   return (await ndbs).concat(await extraNdbs);
 };
